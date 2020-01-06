8519638067e1c4a4e1818a100412f6bc354df3bb
louishsu <is.louishsu@foxmail.com>
2020-01-06 17:40:20 +0800

    Time: 2020-01-06 17:40:20

--- download.sh
+++ download.sh
@@ -5,7 +5,7 @@
  # @Author: louishsu
  # @E-mail: is.louishsu@foxmail.com
  # @Date: 2020-01-05 17:12:46
- # @LastEditTime : 2020-01-05 17:15:57
+ # @LastEditTime : 2020-01-06 17:39:05
  # @Update: 
  ###
 
@@ -17,5 +17,5 @@ wget -O weights/PNet.weights https://github.com/isLouisHsu/MTCNN_Darknet/release
 wget -O weights/RNet.weights https://github.com/isLouisHsu/MTCNN_Darknet/releases/download/v1.0/RNet.weights
 wget -O weights/ONet.weights https://github.com/isLouisHsu/MTCNN_Darknet/releases/download/v1.0/ONet.weights
 
-wget -O ckpt/MobileFacenet.pkl        https://github.com/isLouisHsu/MobileFaceNet_ArcFace_Darknet/blob/master/pretrained/MobileFacenet_best.pkl
-wget -O weights/mobilefacenet.weights https://github.com/isLouisHsu/Mobilefacenet_ArcFace_Darknet/blob/master/weights/mobilefacenet.weights
+wget -O ckpt/MobileFacenet.pkl        https://github.com/isLouisHsu/MobileFaceNet_ArcFace_Darknet/releases/download/v1.0/MobileFacenet_best.pkl
+wget -O weights/mobilefacenet.weights https://github.com/isLouisHsu/MobileFaceNet_ArcFace_Darknet/releases/download/v1.0/mobilefacenet.weights
